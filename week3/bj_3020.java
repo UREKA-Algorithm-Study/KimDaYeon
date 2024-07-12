3541b2485601ec7fb652172503fe095b78b885bf
kimdayeon37 <kdy37912@naver.com>
2024-07-12 17:06:50 +0900

    README UPDATE

--- week3/bj_3020.java
+++ week3/bj_3020.java
@@ -18,28 +18,28 @@ public class Main4 {
         int min=Integer.MAX_VALUE;
         int cnt=0;
 
-        for (int i = 0; i < h; i++) {
+        for (int i = 1; i <= h; i++) {
             int d = binarySearch(down, i);
-//            int u = binarySearch(up, h-i+1);
-//
-//            if(min > d+u) {
-//                min = d+u;
-//                cnt=1;
-//            } else if (min == d+u) cnt++;
+            int u = binarySearch(up, h-i+1);
+
+            if(min > d+u) {
+                min = d+u;
+                cnt=1;
+            } else if (min == d+u) cnt++;
         }
-//        System.out.println(min+ " "+cnt);
+        System.out.println(min+ " "+cnt);
     }
-//    public static int binarySearch(int[] stone, int h) {
-//        int start=0;
-//        int end=n/2-1;
-//
-//        while(start<=end) {
-//            int mid = (start+end)/2;
-//
-//            if (stone[mid]>=h) {
-//                end=mid-1;
-//            } else start=mid+1;
-//        }
-//        return stone.length-start;
-//    }
+    public static int binarySearch(int[] stone, int h) {
+        int start=0;
+        int end=n/2-1;
+
+        while(start<=end) {
+            int mid = (start+end)/2;
+
+            if (stone[mid]>=h) {
+                end=mid-1;
+            } else start=mid+1;
+        }
+        return stone.length-start;
+    }
 }
